1923afb66f1ef603c83bd94c4c366c8aaf625349
Elvis Otoli <otolielvis001@gmail.com>
2026-05-09 18:55:22 +0300

    Fix: Handle Exited with status 1 in cloud deployment

--- app/__init__.py
+++ app/__init__.py
@@ -26,6 +26,15 @@ except ImportError:
         DB_USER = os.getenv('MYSQL_USER')
         DB_PASSWORD = os.getenv('MYSQL_PASSWORD')
         DB_NAME = os.getenv('MYSQL_DB')
+        DB_PORT = os.getenv('MYSQL_PORT', '23648')
+        
+        MYSQL_HOST = os.getenv('MYSQL_HOST')
+        MYSQL_USER = os.getenv('MYSQL_USER')
+        MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+        MYSQL_DB = os.getenv('MYSQL_DB')
+        MYSQL_PORT = os.getenv('MYSQL_PORT', '23648')
+
+        SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
 from .db import init_db
  
